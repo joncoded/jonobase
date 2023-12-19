2d34a5d6458bca19706d8690ad461acb4bde71f2
Joncoded <joncoded@gmail.com>
2023-12-19 11:11:40 -0500

    adding loading/error pages

---
+++ app/(root)/not-found.tsx
@@ -0,0 +1,10 @@
+import { Sect } from "@/components/main"
+
+export default function Loading() {
+  return (
+    <Sect>
+      <h1 className="spec-head font-sans font-bold text-7xl">404</h1>
+      <p className="text-3xl mt-10">Page not found!</p>
+    </Sect>
+  )
+}
---
+++ components/load.tsx
@@ -0,0 +1,10 @@
+import { Sect } from "@/components/main"
+
+export default function Main() {
+  return (
+    <Sect>
+      <h1 className="spec-head font-sans font-bold text-7xl">Coming!</h1>
+      <p className="text-3xl mt-10">Page is loading shortly ... thanks for your patience!</p>
+    </Sect>
+  )
+}
