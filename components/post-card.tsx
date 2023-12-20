42918479b403d5e9e532c07de2be7d242ecd43e0
Joncoded <joncoded@gmail.com>
2023-12-20 10:01:06 -0500

    updating post-card linkage

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -15,7 +15,34 @@ const PostCard = ({post : { slug, image, title, category, subtitle, link, date}}
     )
   }
 
-  const CardHeader = () => {
+  const CardHeader = ({children} : UtilDOMChildrenProps) => {
+    return (
+      <div className="flex flex-col gap-5 p-5">
+        {children}
+      </div>
+    )
+  }
+
+  const CardTitle = () => {
+    return (
+      <div className="card-title">
+        <h3 className="font-sans text-2xl md:text-4xl font-semibold">
+          {title}
+        </h3>
+      </div>      
+    )
+  }
+
+
+  const CardDate = () => {
+    return (
+      <div className={`card-date text-sm`}>
+        {date.substring(0, 10)} {date.substring(11, 16)}
+      </div>
+    )
+  }
+
+  const CardImage = () => {
     return (      
       <>
         {image && 
@@ -33,17 +60,7 @@ const PostCard = ({post : { slug, image, title, category, subtitle, link, date}}
       </>     
     )
   }
-
-  const CardTitle = () => {
-    return (
-      <div className="card-title">
-        <h3 className="font-sans text-2xl md:text-4xl font-semibold">
-          <Link href={`/posts/${slug.current}`}>{title}</Link>
-        </h3>
-      </div>      
-    )
-  }
-
+  
   const CardMeta = ({children}: UtilDOMChildrenProps) => {
     return (
       <div className="flex flex-col p-5 gap-2">
@@ -75,22 +92,14 @@ const PostCard = ({post : { slug, image, title, category, subtitle, link, date}}
     )
   }
 
-  const CardDate = () => {
-    return (
-      <div className={`card-date text-sm`}>
-        {date.substring(0, 10)} {date.substring(11, 16)}
-      </div>
-    )
-  }
-
   return (    
     <Card className="w-full border !bg-transparent">
-      <CardMeta>
-        <CardTitle />
-        <CardDate />
-      </CardMeta>      
       <Link href={`/posts/${slug.current}`}>
-        <CardHeader />        
+        <CardHeader>
+          <CardTitle />
+          <CardDate />
+        </CardHeader>        
+        <CardImage />        
       </Link>      
       <CardMeta>        
         <CardSubtitle />
