72b5434ccbbb2bc12d9a11cef72d2d1ab6d09982
Joncoded <joncoded@gmail.com>
2024-03-21 00:35:59 -0400

    updating post schema

--- sanity/schemas/post.schema.ts
+++ sanity/schemas/post.schema.ts
@@ -37,7 +37,7 @@ const schema = {
       type: 'string',      
       validation: (Rule: any) => Rule.required(),
       options: {
-        list: ['coded', 'noted']
+        list: ['craft', 'notes']
       }
     },
     {
