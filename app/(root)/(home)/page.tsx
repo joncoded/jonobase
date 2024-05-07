93621849ca58a40db0de4cffe1def559d930da0a
jonchius <jonchius@gmail.com>
2024-05-07 17:31:23 -0400

    testing forked repo

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@
 /*
 jonanity by @joncoded
 /app/(root)/(home)/page.tsx
-the homepage
+the root homepage
 */
 
 import { getBase, getList, getPosts } from "@/sanity/actions"
@@ -55,7 +55,7 @@ export default async function Home({ searchParams }: FindProps) {
         <div className={`
           w-3/4 md:w-full max-w-screen-lg mx-auto prose-h2:font-sans prose-p:font-serif
           prose prose-h2:text-4xl md:prose-h2:text-5xl prose-h2:mb-5 prose-p:text-2xl md:prose-p:text-3xl dark:prose-headings:!text-white dark:prose-p:!text-white dark:prose-strong:!text-white`
-        }>
+        }>          
           <PortableText value={intro} />
         </div>
       </div>
