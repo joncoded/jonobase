a271b802177f23520433b31d415a13984cd279c4
jonchius <jonchius@gmail.com>
2024-06-11 13:51:20 -0400

    making responsive "for-now"

--- components/post-list.tsx
+++ components/post-list.tsx
@@ -9,16 +9,30 @@ import PostCard from "./post-card";
 
 export default function PostList({posts} : any) {
 
+  const postsDes = posts
+  const postsTab = posts.slice(0, posts.length % 2 == 1 ? posts.length - 1 : posts.length)
+  const postsMob = posts.slice(0, 2)
+  
   return (
     <div className="post-list w-full sm:justify-start">
       {posts?.length > 0 && 
-        
-        <div className="post-list-item grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-center mt-0">
-          {posts.map((post: any) => (
-            <PostCard key={post._id} post={post} />
-          ))}
-        </div>
-        
+        <>
+          <div className="post-list-item-des hidden lg:grid lg:grid-cols-3 gap-10 justify-center mt-0">
+            {postsDes.map((post: any) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
+          <div className="post-list-item-tab hidden sm:grid lg:hidden sm:grid-cols-2 gap-5 justify-center mt-0">
+            {postsTab.map((post: any) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
+          <div className="post-list-item-mob grid grid-cols-1 sm:hidden gap-5 justify-center mt-0">            
+            {postsMob.map((post: any) => (
+              <PostCard key={post._id} post={post} />
+            ))}
+          </div>
+        </>        
       }
     </div>
   )
