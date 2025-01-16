29a69ca1105bb18378cb99234a669706aa3bc6d7
jonchius <jonchius@gmail.com>
2025-01-16 11:49:03 -0500

    repairing comment bug

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -90,7 +90,7 @@ export default async function Home({ searchParams }: FindProps) {
         <HomeHead />
       </Sect>
       
-      /* the featured posts as defined by the "featured posts" field in the "base" content model */
+      {/* the featured posts as defined by the "featured posts" field in the "base" content model */}
       { featuredPosts && 
       <Sect className={`home-featured bg-amber-300 dark:bg-gray-800 py-5`}>
         <h2 className={`mb-10 font-sans font-bold uppercase text-4xl md:text-5xl text-center`}>
@@ -100,7 +100,7 @@ export default async function Home({ searchParams }: FindProps) {
       </Sect> 
       }
 
-      /* the latest content from the "post" content model */
+      {/* the latest content from the "post" content model */}
       { posts && <Sect className={`home-post pt-5 pb-10`}>
           <h2 className={`mb-10 font-sans font-bold uppercase text-4xl md:text-5xl text-center`}>
             <span aria-hidden="true">[</span> {base.latestPostsTitle} <span aria-hidden="true">]</span>
@@ -112,7 +112,7 @@ export default async function Home({ searchParams }: FindProps) {
         </Sect> 
       }
 
-      /* horizontal sections of "posts" that have "nooks" as defined by the "filters" field in the "base" content model */
+      {/* horizontal sections of "posts" that have "nooks" as defined by the "filters" field in the "base" content model */}
       { filters && filters.map((section: any, index: number) => {
 
         return (
