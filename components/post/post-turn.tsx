1cd4a932eee57de88a529393d669a7d897bb4571
jonchius <jonchius@gmail.com>
2025-03-21 11:11:57 -0400

    switching page-turn direction

--- components/post/post-turn.tsx
+++ components/post/post-turn.tsx
@@ -15,23 +15,23 @@ export default function PostTurn( { newer, turnTitle, older } : { newer?: PostAd
   const linkColors = `${colors.link} dark:${colors.darkLink} hover:${colors.linkHover} dark:hover:${colors.darkLinkHover} hover:underline`
 
   return (
-    <div className={`flex flex-col md:flex-row ${newer ? `justify-between` : `justify-end`} gap-5`}>
-      { newer &&
-        <div className={`flex-1 text-center md:text-left`}>
-          <strong>⬅️ {text['newer']}</strong> ({turnTitle}) <br />
-          <span className={`hover:no-underline`}>{newer.emoji} </span>
-          <Link className={linkColors} href={`/${newer.join}/${newer.kind}/${newer.slug}`}>
-            {newer.title}
-          </Link>
-        </div>
-      }
+    <div className={`flex flex-col md:flex-row ${older ? `justify-between` : `justify-end`} gap-5`}>
       { older &&
-        <div className={`flex-1 text-center md:text-right`}>
-          <strong>{text['older']}</strong> ({turnTitle}) ➡️<br />
+        <div className={`flex-1 text-center md:text-left`}>
+          <strong>⬅️ {text['older']}</strong> ({turnTitle}) <br />
+          <span className={`hover:no-underline`}>{older.emoji} </span>
           <Link className={linkColors} href={`/${older.join}/${older.kind}/${older.slug}`}>
             {older.title}
           </Link>
-          <span className={`hover:no-underline`}> {older.emoji}</span>
+        </div>
+      }
+      { newer &&
+        <div className={`flex-1 text-center md:text-right`}>
+          <strong>{text['newer']}</strong> ({turnTitle}) ➡️<br />
+          <Link className={linkColors} href={`/${newer.join}/${newer.kind}/${newer.slug}`}>
+            {newer.title}
+          </Link>
+          <span className={`hover:no-underline`}> {newer.emoji}</span>
         </div>
       }
     </div>
