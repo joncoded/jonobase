4aa87e6634fbc83a8a609833e6b439a3c69e1789
jonchius <jonchius@gmail.com>
2025-03-22 07:14:30 -0400

    reinstating sanity structuretool

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...index]]/page.tsx` route
  */
 
+import { structureTool } from "sanity/structure"
 import {visionTool} from "@sanity/vision"
 import {defineConfig} from "sanity"
 import {codeInput} from "@sanity/code-input"
@@ -20,8 +21,7 @@ export default defineConfig({
   plugins: [
     codeInput(),    
     table(),
-    // Vision is a tool that lets you query your content with GROQ in the studio
-    // https://www.sanity.io/docs/the-vision-plugin
+    structureTool(), 
     visionTool({defaultApiVersion: apiVersion}),
   ],
 })
