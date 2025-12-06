2e3fcad74c64933474b7149e800a904c9554d578
joncoded <joncoded@gmail.com>
2025-12-06 18:18:34 -0500

    patching lite-dark bug

--- components/base/util/lite-dark.tsx
+++ components/base/util/lite-dark.tsx
@@ -7,7 +7,7 @@ the next.js themes to lite/dark mode provider
 */
 
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes/dist/types"
+import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
